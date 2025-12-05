aff2ac98bca7d9c4de7fd1c2c5143596e2104caf
AxelBT <axelboukari@gmail.com>
2025-12-05 07:25:20 +0100

    form corrigé

--- src/views/mail.js
+++ src/views/mail.js
@@ -2,6 +2,7 @@ function sendmail(){
     let parms = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
+        content : document.getElementById("content").value,
     }
     emailjs.send("service_jcgqqj7", "template_si1in7m", parms).then(alert("Email Sent !"));
 }
